3980bd7aae78ad8231855443bdc940db83d8d829
DanielAlexanderTorres817 <159861897+DanielAlexanderTorres817@users.noreply.github.com>
2024-11-25 10:09:27 -0500

    Updated header

--- Platformer/Title.cpp
+++ Platformer/Title.cpp
@@ -1,3 +1,14 @@
+/**
+* Author: Daniel Torres
+* Assignment: Platformer
+* Date due: 2024-11-25, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
 #include "Title.h"
 #include "Utility.h"
 
