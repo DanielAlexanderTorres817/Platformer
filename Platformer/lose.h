10a1ea3cb5841a5a34bbae8111165a88acda84df
DanielAlexanderTorres817 <159861897+DanielAlexanderTorres817@users.noreply.github.com>
2024-11-25 10:13:39 -0500

    Updated header

--- Platformer/lose.h
+++ Platformer/lose.h
@@ -1,14 +1,24 @@
+/**
+* Author: Daniel Torres
+* Assignment: Platformer
+* Date due: 2024-11-25, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #include "Scene.h"
 
 class Lose : public Scene {
 public:
-    // ————— STATIC ATTRIBUTES ————— //
+    // â€”â€”â€”â€”â€” STATIC ATTRIBUTES â€”â€”â€”â€”â€” //
     int ENEMY_COUNT = 0;
 
-    // ————— DESTRUCTOR ————— //
+    // â€”â€”â€”â€”â€” DESTRUCTOR â€”â€”â€”â€”â€” //
     ~Lose();
 
-    // ————— METHODS ————— //
+    // â€”â€”â€”â€”â€” METHODS â€”â€”â€”â€”â€” //
     void initialise() override;
     void update(float delta_time) override;
     void render(ShaderProgram* program) override;
