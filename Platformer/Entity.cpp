6158af477ce9f4de83de58ac799ccce8a67729e7
DanielAlexanderTorres817 <159861897+DanielAlexanderTorres817@users.noreply.github.com>
2024-11-25 10:01:18 -0500

    updated the header

--- Platformer/Entity.cpp
+++ Platformer/Entity.cpp
@@ -1,3 +1,15 @@
+/**
+* Author: Daniel Torres
+* Assignment: Platformer
+* Date due: 2024-11-25, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 
