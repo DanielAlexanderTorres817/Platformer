58e0981df866699f66d2e33534a66f24d7c6264e
DanielAlexanderTorres817 <159861897+DanielAlexanderTorres817@users.noreply.github.com>
2024-11-25 10:07:41 -0500

    Updated header

--- Platformer/Map.cpp
+++ Platformer/Map.cpp
@@ -1,7 +1,7 @@
 /**
 * Author: Daniel Torres
-* Assignment: Rise of the AI
-* Date due: 2024-11-9, 11:59pm
+* Assignment: Platformer
+* Date due: 2024-11-25, 11:59pm
 * I pledge that I have completed this assignment without
 * collaborating with anyone else, in conformance with the
 * NYU School of Engineering Policies and Procedures on
