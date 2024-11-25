4386108a063318b0372ef465b04e5b52accef065
DanielAlexanderTorres817 <159861897+DanielAlexanderTorres817@users.noreply.github.com>
2024-11-25 10:14:01 -0500

    Updated header

--- Platformer/win.h
+++ Platformer/win.h
@@ -1,15 +1,25 @@
+/**
+* Author: Daniel Torres
+* Assignment: Platformer
+* Date due: 2024-11-25, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #pragma once
 #include "Scene.h"
 
 class Win : public Scene {
 public:
-    // ————— STATIC ATTRIBUTES ————— //
+    // â€”â€”â€”â€”â€” STATIC ATTRIBUTES â€”â€”â€”â€”â€” //
     int ENEMY_COUNT = 0;
 
-    // ————— DESTRUCTOR ————— //
+    // â€”â€”â€”â€”â€” DESTRUCTOR â€”â€”â€”â€”â€” //
     ~Win();
 
-    // ————— METHODS ————— //
+    // â€”â€”â€”â€”â€” METHODS â€”â€”â€”â€”â€” //
     void initialise() override;
     void update(float delta_time) override;
     void render(ShaderProgram* program) override;
